f9e39af70f5370f147d85845b15867c6e3599769
KyuCheol Hwang <97iambigman@gmail.com>
2023-04-26 16:29:32 +0900

    feat(domain/Lotto) : 로또 번호가 중복되는지 검증하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Lotto {
 
     public static final int PRICE = 1000;
+    public static final int SIZE = 6;
 
     private final List<Integer> numbers;
 
@@ -13,11 +14,21 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    /** 비즈니스 로직 **/
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != SIZE || hasDuplicatedNumbers(numbers)) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasDuplicatedNumbers(List<Integer> numbers) {
+        boolean[] used = new boolean[46];
+        for (int number : numbers) {
+            if (used[number]) {
+                return true;
+            }
+            used[number] = true;
+        }
+        return false;
+    }
 }
