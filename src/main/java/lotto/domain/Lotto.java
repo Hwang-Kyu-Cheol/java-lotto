164b62bb2e5d94a9937663f8613e0d028ab997de
KyuCheol Hwang <97iambigman@gmail.com>
2023-04-26 14:35:39 +0900

    reset

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,29 +10,11 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-
-    public boolean contains(Integer number) {
-        return numbers.contains(number);
-    }
-
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6 || hasDuplicatedNumbers(numbers)) {
+        if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
     // TODO: 추가 기능 구현
-    private boolean hasDuplicatedNumbers(List<Integer> numbers) {
-        boolean[] used = new boolean[46];
-        for (Integer number : numbers) {
-            if (used[number]) {
-                return true;
-            }
-            used[number] = true;
-        }
-        return false;
-    }
 }
