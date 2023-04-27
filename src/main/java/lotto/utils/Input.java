71c125845758e15986cdfaa9a0ba6ae043fec345
KyuCheol Hwang <97iambigman@gmail.com>
2023-04-27 14:37:13 +0900

    feat(utils/Input) : 로또 당첨 번호를 입력받고, 번호를 List<Integer>로 변환하는 기능 구현

--- src/main/java/lotto/utils/Input.java
+++ src/main/java/lotto/utils/Input.java
@@ -2,14 +2,25 @@ package lotto.utils;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class Input {
 
     /**
-     * 로또 구입 금액을 입력받고, 금액을 int로 반환하는 기능
+     * 로또 구입 금액을 입력받고, 금액을 int로 변환하는 기능
      * @return int
      */
     public static int getCost() {
         String input = Console.readLine();
         return Resolver.resolveCost(input);
+    }
+
+    /**
+     * 로또 당첨 번호를 입력받고, 번호를 List<Integer>로 변환하는 기능
+     * @return List<Integer>
+     */
+    public static List<Integer> getWinningNumbers() {
+        String input = Console.readLine();
+        return Resolver.resolveWinningNumbers(input);
     }
 }
