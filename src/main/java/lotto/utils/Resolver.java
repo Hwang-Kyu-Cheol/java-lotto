e36852791c34794a629a608e6a4d069b7b3c9bcd
KyuCheol Hwang <97iambigman@gmail.com>
2023-04-27 14:34:33 +0900

    feat(utils/Resolver) : 당첨 번호 String을 int로 변환하는 기능 구현

--- src/main/java/lotto/utils/Resolver.java
+++ src/main/java/lotto/utils/Resolver.java
@@ -1,5 +1,11 @@
 package lotto.utils;
 
+import lotto.constant.Constant;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Resolver {
 
     /**
@@ -10,5 +16,17 @@ public class Resolver {
     public static int resolveCost(String input) {
         Validator.validateCost(input);
         return Integer.parseInt(input);
+    }
+
+    /**
+     * 로또 당첨 번호(String)를 List<Integer>로 변환하는 기능
+     * @param input
+     * @return List<Integer>
+     */
+    public static List<Integer> resolveWinningNumbers(String input) {
+        Validator.validateWinningNumbers(input);
+        return Arrays.stream(input.split(Constant.DELIMITER))
+                .map(winningNumber -> Integer.parseInt(winningNumber))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/lotto/utils/ResolverTest.java
+++ src/test/java/lotto/utils/ResolverTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +22,18 @@ class ResolverTest {
 
         //then
         assertThat(cost).isEqualTo(1000);
+    }
+
+    @DisplayName("\"1,2,3,4,5,6\"이 주어질 경우, 숫자가 담긴 리스트로 변환합니다.")
+    @Test
+    void resolveStringToIntegerList() {
+        //given
+        String input = "1,2,3,4,5,6";
+
+        //when
+        List<Integer> winningNumbers = Resolver.resolveWinningNumbers(input);
+
+        //then
+        assertThat(winningNumbers).isEqualTo(List.of(1,2,3,4,5,6));
     }
 }
